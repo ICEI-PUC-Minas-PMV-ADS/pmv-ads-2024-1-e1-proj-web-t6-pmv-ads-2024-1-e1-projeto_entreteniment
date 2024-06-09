f1f40c6b760be2990b308daab562002ffabac0bf
DanielLaurindoMed <161989652+DanielLaurindoMed@users.noreply.github.com>
2024-06-09 16:10:51 -0300

    mudança js

--- codigo-fonte/tela-incial/garramo.js
+++ codigo-fonte/tela-incial/garramo.js
@@ -1,22 +1,84 @@
-function toggleVideo(){
-    const trailer = document.querySelector('.trailer');
-    const video = document.querySelector('video');
-    video.pause();
-    trailer.classList.toggle('active');
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
 
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
 }
 
-function changeBg(bg, title) {
-    const banner = document.querySelector ('.banneer');
-    const contents = document.querySelector ('.contentt');
-    banner.style.background= `url("../img/moves/${bg}")`;
-    banner.style.backgroundSize = 'cover';
-    banner.style.backgroundPosition = 'center';
 
-    contents.forEach(contentt => {
-        contentt.classList.remove('active');
-        if(contentt.classList.contains(title)) {
-            contentt.classList.add('active');
-        }   
-    });
-}
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
+
+  var swiper = new Swiper(".anime-slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true
+  });
+
+
+  var swiper = new Swiper(".action-slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true
+  });
+
+
+  var swiper = new Swiper(".child-slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true
+  });
+
+  var swiper = new Swiper(".family-slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true
+  });

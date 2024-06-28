924efef9cf3fde8e58d793c95c22e6f4cfa3775e
Dan <161989652+DanielLaurindoMed@users.noreply.github.com>
2024-06-28 18:40:18 -0300

    Update garramo.js

--- codigo-fonte/tela-incial/garramo.js
+++ codigo-fonte/tela-incial/garramo.js
@@ -82,3 +82,41 @@ var swiper = new Swiper(".home-slider", {
     },
     loop:true
   });
+
+// Função para recomendação de filmes aleatórios
+function recomendarFilmes() {
+  // Categorias de filmes disponíveis
+  const categorias = ['Todos', 'Fantasia', 'Aventura', 'Romance'];
+
+  // Loop por cada categoria
+  categorias.forEach(categoria => {
+      // Seleciona aleatoriamente um filme da categoria
+      const filmes = document.querySelectorAll(`#${categoria.toLowerCase()} .swiper-slide`);
+      const filmeAleatorio = filmes[Math.floor(Math.random() * filmes.length)];
+
+      // Adiciona a classe 'recomendado' para destacar o filme recomendado
+      filmeAleatorio.classList.add('recomendado');
+  });
+}
+
+// Chamada da função para recomendar filmes ao carregar a página
+document.addEventListener('DOMContentLoaded', () => {
+  recomendarFilmes();
+});
+
+function search(){
+  let input = document.getElementById('searchbar').value 
+  input = input.toLowerCase()
+  let x = document.getElementsByClassName('film')
+
+  for(i= 0; i < x.length; i++){
+
+    if(!x[i].innerHTML.toLowerCase().includes(input)){
+      x[i].style.display = "none"
+    } else{
+      x[i].style.display = "list-item"
+    }
+
+  }
+
+}

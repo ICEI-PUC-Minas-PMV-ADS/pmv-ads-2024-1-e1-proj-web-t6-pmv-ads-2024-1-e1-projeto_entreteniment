4f2762c7f0b36a5dd76d5e0cf48915e2212c1570
Andre Samuel <andresamueltf@gmail.com>
2024-06-27 21:46:35 -0300

    login js

--- codigo-fonte/script.js
+++ codigo-fonte/script.js
@@ -24,8 +24,16 @@ function logar(){
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
-    if (login == "admin" && senha == "admin") {
-        alert('sucesso');
-    }
+    if (login == "andre" && senha == "andre1402") {
+        alert('Sucesso');
+        location.href = "tela-incial/inicial.html";
+    } else if (login == "daniel" && senha == "daniel2024") {
+        alert('Sucesso');
+        location.href = "tela-incial/inicial.html";
+    }else{
+        alert('Usuário ou senha incorretos');
+        location.href == "index.html"
+
+    }   
 
 }
